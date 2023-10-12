e19d1f1aa9774a43dfcee9f3fd6c957038510cea
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2023-10-12 16:08:39 -0300

    Aula 01 finalizada

---
+++ todolist/src/main/java/br/com/gustavogogola/todolist/user/UserController.java
@@ -0,0 +1,35 @@
+package br.com.gustavogogola.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ *  public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    /**
+     * String
+     * Integer (Int)
+     * Double 
+     * float
+     * Char 
+     * Date
+     * void
+     */
+    /**
+     *  Body
+     * @param userModel
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+}
