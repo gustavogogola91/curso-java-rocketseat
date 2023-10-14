e504474f999b17a45edd32e0d7a3584e07f9b62b
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2023-10-14 19:26:28 -0300

    Aula 04

--- todolist/src/main/java/br/com/gustavogogola/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/gustavogogola/todolist/task/TaskController.java
@@ -1,16 +1,21 @@
 package br.com.gustavogogola.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.gustavogogola.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -26,13 +31,38 @@ public class TaskController {
         taskModel.setIdUser((UUID) idUser);
 
         var currentDate = LocalDateTime.now();
-        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio / término deve ser maior que a data atual");
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio deve ser maior que a data de término.");
         }
 
-        
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de inicio / término deve ser maior que a data atual");
+        }
+
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel updateTask(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperty(taskModel, task);
+
+        return this.taskRepository.save(task);
 
     }
 }
